fed8bf948568fcddc4dc70c0e108649934da8754
Italo Alves <italooko@gmail.com>
2020-04-03 21:43:06 -0300

    Casting int variable passed via POST

--- inc/misha-load-more-ajax.php
+++ inc/misha-load-more-ajax.php
@@ -4,21 +4,22 @@
  */
 function misha_loadmore_ajax_handler() {
   
-	// prepare our arguments for the query
-	$args = json_decode( stripslashes( $_POST['query'] ), true );
-	$args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
-	$args['post_status'] = 'publish';
+  // prepare our arguments for the query
+  $args = json_decode( stripslashes( $_POST['query'] ), true );
+  $args['paged'] = intval( $_POST['page'] ) + 1; // we need next page to be loaded
+  $args['post_status'] = 'publish';
   
-	// it is always better to use WP_Query but not here
-	query_posts( $args );
-	
+  // it is always better to use WP_Query but not here
+  query_posts( $args );
+
   if ( have_posts() ) :
     while( have_posts() ): the_post();
       set_query_var( 'page_num', $args['paged'] );
       get_template_part( 'content', 'post-full' );
     endwhile;
-	endif;
-	die; // here we exit the script and even no wp_reset_query() required!
+  endif;
+  
+  die; // here we exit the script and even no wp_reset_query() required!
 }
 add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
 add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
